22eca112e1c910797f20cb85297bb3331b905fc7
Anthony Mandra <amandra@gmail.com>
2018-01-20 14:19:41 -0500

    squash

--- app/src/main/java/com/anthonymandra/rawdroid/ui/GalleryViewHolder.kt
+++ app/src/main/java/com/anthonymandra/rawdroid/ui/GalleryViewHolder.kt
@@ -10,11 +10,10 @@ import com.anthonymandra.rawdroid.R
 import com.anthonymandra.rawdroid.data.MetadataResult
 import com.anthonymandra.util.MetaUtil
 import com.bumptech.glide.Glide
-import com.bumptech.glide.RequestManager
 import kotlinx.android.synthetic.main.fileview.view.*
 
 @Suppress("DEPRECATION")
-class GalleryViewHolder(view: View, private val glide: RequestManager) : RecyclerView.ViewHolder(view) {
+class GalleryViewHolder(view: View/*, private val glide: RequestManager*/) : RecyclerView.ViewHolder(view) {
     private var image: MetadataResult? = null
 
     private val purple: Int = view.resources.getColor(R.color.startPurple)
@@ -59,10 +58,10 @@ class GalleryViewHolder(view: View, private val glide: RequestManager) : Recycle
     }
 
     companion object {
-        fun create(parent: ViewGroup, glide: RequestManager): GalleryViewHolder {
+        fun create(parent: ViewGroup/*, glide: RequestManager*/): GalleryViewHolder {
             val view = LayoutInflater.from(parent.context)
                 .inflate(R.layout.fileview, parent, false)
-            return GalleryViewHolder(view, glide)
+            return GalleryViewHolder(view/*, glide*/)
         }
     }
 }
