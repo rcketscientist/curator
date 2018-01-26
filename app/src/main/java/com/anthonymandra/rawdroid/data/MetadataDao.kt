3e42dd4cbbd0faee865d34c4cb2e159046acf985
Anthony Mandra <anthony.mandra@keysight.com>
2018-01-26 16:46:05 -0500

    Squash: Handle selection logic in adapter.

--- app/src/main/java/com/anthonymandra/rawdroid/data/MetadataDao.kt
+++ app/src/main/java/com/anthonymandra/rawdroid/data/MetadataDao.kt
@@ -1,12 +1,15 @@
 package com.anthonymandra.rawdroid.data
 
 import android.arch.lifecycle.LiveData
-import android.arch.persistence.room.Dao
-import android.arch.persistence.room.Delete
-import android.arch.persistence.room.Insert
-import android.arch.persistence.room.OnConflictStrategy
-import android.arch.persistence.room.Query
-import android.arch.persistence.room.Update
+import android.arch.paging.DataSource
+import android.arch.paging.LivePagedListProvider
+import android.arch.persistence.db.SupportSQLiteQuery
+import android.arch.persistence.db.SupportSQLiteQueryBuilder
+import android.arch.persistence.room.*
+import com.anthonymandra.content.Meta
+import com.anthonymandra.rawdroid.XmpFilter
+import com.anthonymandra.util.DbUtil
+import java.util.ArrayList
 
 @Dao
 abstract class MetadataDao {
@@ -28,6 +31,12 @@ abstract class MetadataDao {
 
     @Query("SELECT COUNT(*) FROM meta")
     abstract fun count(): Int
+
+    @RawQuery
+    internal abstract fun getImages(query: SupportSQLiteQuery): LiveData<List<MetadataEntity>>
+
+    @RawQuery
+    internal abstract fun getImages2(query: SupportSQLiteQuery): DataSource.Factory<Int, MetadataEntity>
 
     @Query("SELECT * FROM meta " +
         "INNER JOIN image_parent " +
@@ -402,5 +411,77 @@ abstract class MetadataDao {
         private const val SEGREGATE = "type COLLATE NOCASE ASC"
         private const val NAME_ASC = "meta.name COLLATE NOCASE ASC"
         private const val OrderTypeNameAsc = "ORDER BY type COLLATE NOCASE ASC, meta.name COLLATE NOCASE ASC"
+    }
+
+    public fun getImages(filter: XmpFilter) : DataSource.Factory<Int, MetadataEntity> {
+        return getImages2(createFilterQuery(filter))
+    }
+
+    private fun createFilterQuery(filter: XmpFilter): SupportSQLiteQuery {
+        val selection = StringBuilder()
+        val selectionArgs = ArrayList<String>()
+        var requiresJoiner = false
+
+        val and = " AND "
+        val or = " OR "
+        val joiner = if (filter.andTrueOrFalse) and else or
+
+        if (filter.xmp != null) {
+            if (filter.xmp.label.isNotEmpty()) {
+                requiresJoiner = true
+
+                selection.append(DbUtil.createIN(Meta.LABEL, filter.xmp.label.size))
+                selectionArgs.addAll(filter.xmp.label)
+            }
+            //			if (filter.xmp.subject != null && filter.xmp.subject.length > 0)
+            //			{
+            //				if (requiresJoiner)
+            //					selection.append(joiner);
+            //				requiresJoiner = true;
+            //
+            //				selection.append(DbUtil.createLike(Meta.SUBJECT, filter.xmp.subject,
+            //						selectionArgs, joiner, false,
+            //						"%", "%",   // openended wildcards, match subject anywhere
+            //						null));
+            //			}
+            if (filter.xmp.rating.isNotEmpty()) {
+                if (requiresJoiner)
+                    selection.append(joiner)
+                requiresJoiner = true
+
+                selection.append(DbUtil.createIN(Meta.RATING, filter.xmp.rating.size))
+                filter.xmp.rating.mapTo(selectionArgs) { java.lang.Double.toString(it.toDouble()) }
+            }
+        }
+        if (filter.hiddenFolders.isNotEmpty()) {
+            if (requiresJoiner)
+                selection.append(and)  // Always exclude the folders, don't OR
+
+            selection.append(DbUtil.createLike(Meta.PARENT,
+                    filter.hiddenFolders.toTypedArray(),
+                    selectionArgs,
+                    and, // Requires AND so multiple hides don't negate each other
+                    true, null, // No wild to start, matches path exactly
+                    "%", // Wildcard end to match all children
+                    "%")// NOT
+            )  // Uri contain '%' which means match any so escape them
+        }
+
+        val order = if (filter.sortAscending) " ASC" else " DESC"
+        val sort = StringBuilder()
+
+        if (filter.segregateByType) {
+            sort.append(Meta.TYPE).append(" COLLATE NOCASE").append(" ASC, ")
+        }
+        when (filter.sortColumn) {
+            XmpFilter.SortColumns.Date -> sort.append(Meta.TIMESTAMP).append(order)
+            XmpFilter.SortColumns.Name -> sort.append(Meta.NAME).append(" COLLATE NOCASE").append(order)
+        }
+
+        //TODO: We need to set the subject selection
+        return SupportSQLiteQueryBuilder.builder("meta")
+                .selection(selection.toString(), selectionArgs.toArray())
+                .orderBy(sort.toString())
+                .create()
     }
 }
