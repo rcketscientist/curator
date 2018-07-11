5cd86fd52631f53b0f6a56ce1dd0c431744bbdd7
Anthony Mandra <amandra@gmail.com>
2018-07-10 21:23:05 -0400

    mediator doesn't update and isn't needed.

--- app/src/main/java/com/anthonymandra/rawdroid/ui/FilterViewModel.kt
+++ app/src/main/java/com/anthonymandra/rawdroid/ui/FilterViewModel.kt
@@ -3,22 +3,21 @@ package com.anthonymandra.rawdroid.ui
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.MediatorLiveData
 import com.anthonymandra.rawdroid.App
 import com.anthonymandra.rawdroid.data.FolderEntity
 
 class FilterViewModel(app: Application) : AndroidViewModel(app) {
 
     // MediatorLiveData can observe other LiveData objects and react on their emissions.
-    private val mObservableProducts: MediatorLiveData<List<FolderEntity>> = MediatorLiveData()
-    private val dataSource = (app as App).database.folderDao()
+//    private val mObservableProducts: MediatorLiveData<List<FolderEntity>> = MediatorLiveData()
+//    private val dataSource = (app as App).database.folderDao()
     private val dataRepo = (app as App).dataRepo
 
     /**
      * Expose the LiveData keywords query so the UI can observe it.
      */
     val folders: LiveData<List<FolderEntity>>
-        get() = mObservableProducts
+        get() = dataRepo.lifecycleParents
 
     fun insertFolders(vararg folders: FolderEntity) {
         dataRepo.insertParents(*folders).subscribe()
@@ -28,13 +27,13 @@ class FilterViewModel(app: Application) : AndroidViewModel(app) {
         dataRepo.updateParents(*folders).subscribe()
     }
 
-    init {
-        // set by default null, until we get data from the database.
-        mObservableProducts.value = null
-
-        val folders = dataSource.lifecycleParents
-
-        // observe the changes of the products from the database and forward them
-        mObservableProducts.addSource<List<FolderEntity>>(folders, { mObservableProducts.setValue(it) })
-    }
+//    init {
+//        // set by default null, until we get data from the database.
+//        mObservableProducts.value = null
+//
+//        val folders = dataSource.lifecycleParents
+//
+//        // observe the changes of the products from the database and forward them
+//        mObservableProducts.addSource<List<FolderEntity>>(folders, { mObservableProducts.setValue(it) })
+//    }
 }
